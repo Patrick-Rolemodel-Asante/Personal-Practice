dcc3f17b0508f66db4987f1393c84e095ea15f4b
Rol <rolemodellagina@gmail.com>
2023-09-16 07:13:41 +0000

    Revert "make changes"
    This reverts commit cc65e2a955d716dfdcfbad5ef2e12c5a1b76fdc8.

--- python-automation/sendSMS.py
+++ python-automation/sendSMS.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
 account_sid = 'AC5ac77c07699c57fd52b1ab5282b3c61d'
-auth_token = '[63ef08c50b0a4c40c813ab1fd4baf55d]'
+auth_token = '[AuthToken]'
 client = Client(account_sid, auth_token)
 
 def sendSMS():
