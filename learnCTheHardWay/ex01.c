182f3a7285a2d4afa02bee84437d0be4f93ceafc
Rol <alxse19.1@gmail.com>
2023-09-16 07:40:27 +0000

    add files

---
+++ learnCTheHardWay/ex01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*This is a comment*/
+
+int main(int rol, char *rols[])
+{
+int distance = 100;
+
+// this is also a comment
+printf("You are %d miles away\n");
+
+return 0;
+}
